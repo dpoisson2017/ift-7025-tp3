b73f5ba02631d8b7ab9e5084e532de005d4f532f
Julien <julien.lafrance.1@ulaval.ca>
2023-04-07 17:43:25 -0400

    Add base classifier method body

--- classifier.py
+++ classifier.py
@@ -23,6 +23,7 @@ class Classifier: #nom de la class à changer
 		Vous pouvez passer d'autre paramètres au besoin,
 		c'est à vous d'utiliser vos propres notations
 		"""
+		self.possibleClasses:list[str] = []
 		
 		
 	def train(self, train, train_labels): #vous pouvez rajouter d'autres attributs au besoin
@@ -45,7 +46,15 @@ class Classifier: #nom de la class à changer
 		exemple est de taille 1xm
 		"""
         
-	def evaluate(self, X, y):
+	def predictArray(self, data:np.ndarray) -> np.ndarray:
+		results = np.zeros((np.shape(data)[0], 1))
+		i:np.ndarray
+		for index, row in enumerate(data):
+			result = self.predict(row)
+			results[index] = result
+		return results
+
+	def evaluate(self, evaluation_data: np.ndarray, evaluation_labels: np.ndarray):
 		"""
 		c'est la méthode qui va evaluer votre modèle sur les données X
 		l'argument X est une matrice de type Numpy et de taille nxm, avec 
@@ -57,7 +66,50 @@ class Classifier: #nom de la class à changer
 		vous pouvez rajouter d'autres arguments, il suffit juste de
 		les expliquer en commentaire
 		"""
-        
-	
+		rates = {"TP":0, "FP":0, "TN":0, "FN":0}
+		class_recognition = dict()
+		prediction_results = self.predictArray(evaluation_data)
+		for i in self.possibleClasses:
+			class_recognition[i] = rates.copy()
+		for index, result in enumerate(prediction_results):
+			real_class = evaluation_labels[index]
+			if result == real_class:
+				for i in class_recognition:
+					if(i == real_class):
+						class_recognition[real_class]["TP"] += 1
+					else:
+						class_recognition[i]["TN"] += 1
+			else:
+				for i in class_recognition:
+					if(i == real_class):
+						class_recognition[real_class]["FN"] += 1
+					elif(i == result):
+						class_recognition[result]["FP"] += 1
+					else:
+						class_recognition[i]["TN"] += 1
+		
+		v:dict
+		for k, v in class_recognition.items():
+			print(f"Evaluation metrics for {k}\n")
+			accuracy = (v["TP"] + v["TN"])/(sum(v.values))
+			print(f"Accuracy: {accuracy}\n")
+			precision = (v["TP"])/(v["TP"] + v["FP"])
+			print(f"Precision: {precision}\n")
+			recall = (v["TP"])/(v["TP"] + v["FN"])
+			print(f"Recall: {recall}\n")
+			f1score = 2 * ((precision * recall) / (precision + recall))
+			print(f"F1-score: {f1score}\n")
+			print("Confusion matrix \n")
+			matrix = f""" 
+							Predicted
+					Positive	|	Negative
+				|---------------|---------------|
+		Positive|   {v["TP"]}   |   {v["FN"]}   |
+Actual  --------|---------------|---------------|
+				|---------------|---------------|
+		Negative|   {v["FP"]}   |   {v["TN"]}   |
+				|---------------|---------------|"""
+			print(matrix)
+
 	# Vous pouvez rajouter d'autres méthodes et fonctions,
 	# il suffit juste de les commenter.
