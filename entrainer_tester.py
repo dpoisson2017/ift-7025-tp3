cefb589cb6333d5fa83714481c402df1a5321bc1
David Poisson <david.poisson.1@ulaval.ca>
2023-04-10 21:13:39 -0400

    - First draft pour la cross-validation

--- entrainer_tester.py
+++ entrainer_tester.py
@@ -8,12 +8,38 @@ import sklearn.metrics
 
 TRAINING_THRESHOLD = 0.8
 NUMBER_NEAREST_NEIGHBORS = 5
+NUMBER_BINS = 10
+K_MIN = 5
+K_MAX = 10
 
 datasets = ['iris',
             'wine',
             'abalone']
 
+def findBestKValue(dataset):
+    if dataset == 'iris':
+        dataBins = load_datasets.load_iris_dataset_bins(NUMBER_BINS)
+    elif dataset == 'wine':
+        dataBins = load_datasets.load_wine_dataset_bins(NUMBER_BINS)
+    elif dataset == 'abalone':
+        dataBins = load_datasets.load_abalone_dataset_bins(NUMBER_BINS)
 
+    l_errors = list()
+    for kValue in range(K_MIN, K_MAX):
+        print("Calculating for k = " + str(kValue))
+        sumScores = 0
+        for bin in range(NUMBER_BINS):
+            print("Calculating for bin = " + str(bin))
+            train, train_label, test, test_label = load_datasets.dataFromBins(dataBins, NUMBER_BINS, bin)
+            knneighbours = Knn(k=kValue)
+            knneighbours.train(train.astype(float, copy=False), train_label)
+            knneighbours.evaluate(test.astype(float, copy=False), test_label)
+            sumScores += knneighbours.f1_score
+        avgScore = float(sumScores)/NUMBER_BINS
+        l_errors.append((avgScore,kValue))
+
+    # Take the kValue of the lowest score
+    return sorted(l_errors)[0][1]
 
 print("Execution du classificateur Naif Baysien")
 for dataset in datasets:
@@ -51,6 +77,11 @@ print("Execution du classificateur K nearest neighbors (KNN)")
 for dataset in datasets:
     print("Dataset: {}".format(dataset))
 
+    print("Starting cross-validation to find best k-value")
+    bestK = findBestKValue(dataset)
+    print("Best kValue is: " + str(bestK))
+
+
     if dataset == 'iris':
         train, train_label, test, test_label = load_datasets.load_iris_dataset(TRAINING_THRESHOLD)
     elif dataset == 'wine':
@@ -59,7 +90,7 @@ for dataset in datasets:
         train, train_label, test, test_label = load_datasets.load_abalone_dataset(TRAINING_THRESHOLD)
 
     print("Creation KNN classifier")
-    knneighbours = Knn(k=NUMBER_NEAREST_NEIGHBORS)
+    knneighbours = Knn(k=bestK)
     print("Training KNN classifier")
     knneighbours.train(train.astype(float, copy=False), train_label)
     print("Predicting KNN classifier")
@@ -77,3 +108,8 @@ for dataset in datasets:
     print("Evaluating scikit KNeighbors classifier")
     results1 = scikit_knn.predict(test.astype(float, copy=False))
     print(f"mean accuracy with sklearn KNeighbors: {sklearn.metrics.accuracy_score(test_label, results1)}")
+
+
+
+
+
